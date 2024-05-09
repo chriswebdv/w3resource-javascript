5483fbb7df79fa19aa0a2ba264b8599a0e77d1a4
chriswebdv <chriswebdv@gmail.com>
2024-05-09 12:11:09 -0600

    Create 2 type of loops: for and while to do a reverse countdown of 1 - 10

---
+++ main.js
@@ -0,0 +1,15 @@
+// For loop countdown
+
+for(let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+console.log("Lift off!");
+
+let i = 10;
+
+while (i >= 1) {
+    console.log(i);
+    i--
+}
+console.log("second lift off!");
+    
